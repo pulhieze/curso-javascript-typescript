d77dbb34ec34656bd0c17ae1c78f20ab4c09267b
pulhieze <vpmello04@gmail.com>
2025-12-26 11:50:55 -0300

    .

--- seção 4 Arrays/aula 46/index.js
+++ seção 4 Arrays/aula 46/index.js
@@ -1,1 +1,8 @@
-a
+//              -5        -4       -3        -2        -1
+//               0         1        2         3         4
+const nomes = ["Maçã", "Banana", "Limão", "Morango", "Manga"];
+
+// nomes.splice(indice, delete, ele1, ele2, ele3)
+
+const removidos = nomes.splice(0, 3);
+console.log(nomes, removidos);
